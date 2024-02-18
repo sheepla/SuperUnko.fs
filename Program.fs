09ca02c70b7a9942fba4a250d3be64e1d038826d
sheepla <62412884+sheepla@users.noreply.github.com>
2024-02-18 20:02:02 +0900

    Extract logic into KingUnko module

--- Program.fs
+++ Program.fs
@@ -1,79 +1,90 @@
-type Element =
-    | Unko
-    | Crown
-    | Eye
-    | Nose
-    | Mouth
-    | Space
+module KingUnko =
+    type Element =
+        | Unko
+        | Crown
+        | Eye
+        | Nose
+        | Mouth
+        | Space
 
-    member self.ToEmoji() : string =
-        match self with
-        | Unko -> "💩"
-        | Crown -> "👑"
-        | Eye -> "👁"
-        | Nose -> "👃"
-        | Mouth -> "👄"
-        | Space -> "　"
+        member self.ToEmoji() : string =
+            match self with
+            | Unko -> "💩"
+            | Crown -> "👑"
+            | Eye -> "👁"
+            | Nose -> "👃"
+            | Mouth -> "👄"
+            | Space -> "　"
 
-type Pattern =
-    {
-        Element: Element
-        Count: int
-    }
-
-    member self.Replicate() = List.replicate self.Count self.Element
-
-let generate (height: int) (n: int) : Pattern list =
-    let padding =
+    type Pattern =
         {
-            Element = Space
-            Count = (height - (n - 1))
+            Element: Element
+            Count: int
         }
 
-    match n with
-    | 1 -> [ padding; { Element = Crown; Count = 1 }; padding ]
-    | 3 ->
-        [
-            padding
-            { Element = Unko; Count = 1 }
-            { Element = Eye; Count = 1 }
-            { Element = Unko; Count = 1 }
-            { Element = Eye; Count = 1 }
-            { Element = Unko; Count = 1 }
-            padding
-        ]
-    | 4 ->
-        [
-            padding
-            { Element = Unko; Count = 3 }
-            { Element = Nose; Count = 1 }
-            { Element = Unko; Count = 3 }
-            padding
-        ]
-    | 5 ->
-        [
-            padding
-            { Element = Unko; Count = 4 }
-            { Element = Mouth; Count = 1 }
-            { Element = Unko; Count = 4 }
-            padding
-        ]
-    | _ -> [ padding; { Element = Unko; Count = (2 * n - 1) }; padding ]
+        member self.Replicate() = List.replicate self.Count self.Element
 
-let generateStep (height: int) (n: int) =
-    generate height n
-    |> List.map (fun pattern -> pattern.Replicate())
-    |> List.concat
+    let private generate (height: int) (n: int) : Pattern list =
+        let padding =
+            {
+                Element = Space
+                Count = (height - (n - 1))
+            }
 
-let run (height: int) =
-    for n in [ 1..height ] do
-        generateStep height n
-        |> List.map (fun step -> step.ToEmoji())
-        |> String.concat ""
-        |> printfn "%s"
+        match n with
+        | 1 -> [ padding; { Element = Crown; Count = 1 }; padding ]
+        | 3 ->
+            [
+                padding
+                { Element = Unko; Count = 1 }
+                { Element = Eye; Count = 1 }
+                { Element = Unko; Count = 1 }
+                { Element = Eye; Count = 1 }
+                { Element = Unko; Count = 1 }
+                padding
+            ]
+        | 4 ->
+            [
+                padding
+                { Element = Unko; Count = 3 }
+                { Element = Nose; Count = 1 }
+                { Element = Unko; Count = 3 }
+                padding
+            ]
+        | 5 ->
+            [
+                padding
+                { Element = Unko; Count = 4 }
+                { Element = Mouth; Count = 1 }
+                { Element = Unko; Count = 4 }
+                padding
+            ]
+        | _ -> [ padding; { Element = Unko; Count = (2 * n - 1) }; padding ]
 
-[<EntryPoint>]
-let main args =
+    let private generateStep (height: int) (n: int) =
+        generate height n
+        |> List.map (fun pattern -> pattern.Replicate())
+        |> List.concat
+
+    let generateAll (height: int) =
+        for n in [ 1..height ] do
+            generateStep height n
+            |> List.map (fun step -> step.ToEmoji())
+            |> String.concat ""
+            |> printfn "%s"
+
+type ExitStatus =
+    | Ok
+    | InvalidArgs of string
+
+    member self.ToInt() : int =
+        match self with
+        | Ok -> 0
+        | InvalidArgs(args) -> 
+            eprintfn "invalid arguments: %A" args
+            1
+
+let run (args:string array) : ExitStatus =
     match args with
     | args when args.Length = 1 ->
         let tryParseInt (s: string) : int option =
@@ -82,9 +93,15 @@ let main args =
             | _ -> None
 
         match tryParseInt args[0] with
-        | Some(height) -> run height
+        | Some(height) -> KingUnko.generateAll height
         | None -> eprintfn "invalid number: %s" args[0]
 
-    | args -> eprintfn "invalid argument(s): %A" args
+        ExitStatus.Ok
+    | args ->
+        ExitStatus.InvalidArgs <| String.concat " " args
+    
+[<EntryPoint>]
+let main args =
+    let exitStatus = run args
+    exitStatus.ToInt()
 
-    0
